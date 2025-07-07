78745552fef87be7b00b48fc1a30e25ae1ea4b23
bree-nen <99203626+bree-nen@users.noreply.github.com>
2025-07-07 02:06:16 +0200

    Added more weather data(details

--- app.js
+++ app.js
@@ -1,35 +1,62 @@
 function refreshWeather(response) {
-    let temperatureElement = document.querySelector("#temperature");
-    let temperature = response.data.temperature.current;
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
 
-let cityElement = document.querySelector("#city");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let currentTimeElement = document.querySelector("#current-time");
+  let date = new Date(response.data.time * 1000);
 
-cityElement.innerHTML = response.data.city;
+  console.log(response.data);
 
-    temperatureElement.innerHTML= Math.round(temperature);
+  cityElement.innerHTML = response.data.city;
+  currentTimeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes} `;
 }
 
 function searchCity(city) {
-    // make api call and update the interface
-let apiKey = `38b3of7fc064a9ct455e1cb4004bf475`;
-let apiUrl =
-  ` https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-axios.get(apiUrl).then(refreshWeather);
-    
+  // make api call and update the interface
+  let apiKey = `38b3of7fc064a9ct455e1cb4004bf475`;
+  let apiUrl = ` https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
 }
-
 
 function handleSearchSubmit(event) {
   event.preventDefault();
 
   let searchInput = document.querySelector("#search-form-input");
-  
 
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-
 
 searchCity("Thohoyandou");
