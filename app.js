ab4a61fa4529158285a0f576d7f4f4118610987a
bree-nen <99203626+bree-nen@users.noreply.github.com>
2025-07-12 15:14:12 +0200

    Inject the forecast HTML to JavaScript

--- app.js
+++ app.js
@@ -64,7 +64,35 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+//Using js to display the forecast instead of having multiple id's in html.
+
+function displayForecast() {
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">🌬</div>
+            <div class="weather-forecast-temperatures">
+              <div class="weather-forecast-temperature">
+                <strong> 15°</strong>
+              </div>
+              <div class="weather-forecast-temperature">9°</div>
+            </div>
+          </div>
+`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Thohoyandou");
+displayForecast();
